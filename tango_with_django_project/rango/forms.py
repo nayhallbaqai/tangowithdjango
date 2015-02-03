4cf200bbee8b607e373b501b458cfa808e334ff7
unknown <2086029b@BO706-03.ad.dcs.gla.ac.uk>
2015-02-03 13:39:55 +0000

    Chapter 9 - completed

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,7 +1,9 @@
 __author__ = 'Nayhall Baqai'
 
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
@@ -37,10 +39,21 @@ class PageForm(forms.ModelForm):
         def clean(self):
             cleaned_data = self.cleaned_data
             url = cleaned_data.get('url')
-
-        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+            # If url is not empty and doesn't start with 'http://', prepend 'http://'.
             if url and not url.startswith('http://'):
                 url = 'http://' + url
                 cleaned_data['url'] = url
 
             return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
